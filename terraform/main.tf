dcfe816202d4653ddcf6dbf163b192cf46561d26
Prathang <49646334+vijayprabhu04@users.noreply.github.com>
2024-08-29 09:08:25 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2_instance" {
-    ami = "ami-05fb0b8c1424f266b"
+    ami = "ami-0cf2b4e024cdb6960"
     instance_type = "t2.micro"
     tags = {Name = "HTTP_SERVER"}
     key_name = "training"
